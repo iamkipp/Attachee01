4de5b699bed14da5fd1eaf2afc355f9350032135
danielariga <arigamaster@gmail.com>
2025-09-26 16:18:07 +0300

    set up dbcontext and program.cs

---
+++ Attachee01/Data/AppDbContext.cs
@@ -0,0 +1,58 @@
+﻿using Attachee01.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Attachee01.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Department> Departments => Set<Department>();
+        public DbSet<DepartmentRoleAssignment> DepartmentRoleAssignments => Set<DepartmentRoleAssignment>();
+        public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+        protected override void OnModelCreating(ModelBuilder b)
+        {
+            base.OnModelCreating(b);
+
+            // Department
+            b.Entity<Department>(e =>
+            {
+                e.Property(x => x.Name).HasMaxLength(128).IsRequired();
+                e.HasIndex(x => x.Name).IsUnique();
+                e.Property(x => x.Code).HasMaxLength(32);
+            });
+
+            // DepartmentRoleAssignment (User ↔ RoleName ↔ Department)
+            b.Entity<DepartmentRoleAssignment>(e =>
+            {
+                e.Property(x => x.RoleName).HasMaxLength(64).IsRequired();
+                e.HasIndex(x => new { x.UserId, x.DepartmentId, x.RoleName }).IsUnique();
+
+                e.HasOne(x => x.User)
+                    .WithMany()
+                    .HasForeignKey(x => x.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                e.HasOne(x => x.Department)
+                    .WithMany(d => d.Assignments)
+                    .HasForeignKey(x => x.DepartmentId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // RefreshToken (optional but mapped)
+            b.Entity<RefreshToken>(e =>
+            {
+                e.HasIndex(x => x.Token).IsUnique();
+                e.Property(x => x.Token).HasMaxLength(512).IsRequired();
+
+                e.HasOne(x => x.User)
+                    .WithMany()
+                    .HasForeignKey(x => x.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
